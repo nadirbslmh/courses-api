b27bf4ff19f8d32350b1e22da885c179ebf292d2
nadirbasalamah <nadir008basalamah@gmail.com>
2023-04-03 16:05:02 +0700

    feat(tests): add unit tests

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"courses-api/models"
 	"courses-api/utils"
+	"errors"
 	"fmt"
 	"log"
 
@@ -48,4 +49,66 @@ func Migrate() {
 	if err != nil {
 		log.Fatalf("failed to perform database migration: %s\n", err)
 	}
+}
+
+func SeedCategory() (models.Category, error) {
+	var category models.Category = models.Category{
+		Name: "test",
+	}
+
+	result := DB.Create(&category)
+
+	if err := result.Error; err != nil {
+		return models.Category{}, err
+	}
+
+	if err := result.Last(&category).Error; err != nil {
+		return models.Category{}, err
+	}
+
+	return category, nil
+}
+
+func SeedCourse() (models.Course, error) {
+	category, err := SeedCategory()
+
+	if err != nil {
+		return models.Course{}, err
+	}
+
+	var course models.Course = models.Course{
+		Title:       "test",
+		Description: "test",
+		CategoryID:  category.ID,
+		Level:       "test",
+	}
+
+	result := DB.Create(&course)
+
+	if err := result.Error; err != nil {
+		return models.Course{}, err
+	}
+
+	if err := result.Last(&course).Error; err != nil {
+		return models.Course{}, err
+	}
+
+	return course, nil
+}
+
+func CleanSeeders() error {
+	DB.Exec("SET FOREIGN_KEY_CHECKS = 0")
+
+	catErr := DB.Exec("DELETE FROM categories").Error
+	courseErr := DB.Exec("DELETE FROM courses").Error
+
+	var isFailed bool = catErr != nil || courseErr != nil
+
+	if isFailed {
+		return errors.New("cleaning failed")
+	}
+
+	log.Println("seeders are cleaned up successfully")
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -28,10 +29,13 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
